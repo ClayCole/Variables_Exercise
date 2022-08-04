e8f34e1fbf6d5eb5dfc499f3d84dd446e424d9ab
Clay Coleman <claycoleman94@gamil.com>
2022-08-04 11:51:41 -0400

    inital commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Variables_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable declerations 
+            int dogAge;
+            string dogName;
+            bool isAlive;
+            double myDouble;
+            Decimal myDecimal;
+
+            // variable initalizations
+            dogAge = 2;
+            dogName = "Titan";
+            isAlive = true;
+
+            Console.WriteLine($"My dog's name is {dogName},He is turning {dogAge}.");
+
+
+        }
+    }
+}
+
